a6a826cf1497e8a9cd3e9f55f1f4486e9d0ae3c9
Madhon <madhon@madhon.com>
2019-12-04 13:14:41 +0000

    package fixes

--- Flake/DisposableAction.cs
+++ Flake/DisposableAction.cs
@@ -6,10 +6,7 @@
     {
         private readonly Action _action;
 
-        public DisposableAction(Action action)
-        {
-            _action = action ?? throw new ArgumentNullException(nameof(action));
-        }
+        public DisposableAction(Action action) => _action = action ?? throw new ArgumentNullException(nameof(action));
 
         public void Dispose() => _action();
     }
