aa49e3a2ab3b13a037f1662eda70949f056b0827
Madhon <madhon@madhon.co.uk>
2023-09-29 16:46:42 +0100

    feat: Add Conditional for Net Standard vs Net 6.0+ For the UnixEpoch

--- Flake/TimeExtensions.cs
+++ Flake/TimeExtensions.cs
@@ -20,9 +20,16 @@
             return new DisposableAction(() => { currentTimeFunc = InternalCurrentTimeMillis; });
         }
 
+#if NETSTANDARD2_0
         private static readonly DateTime Jan1st1970 = new DateTime
             (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        private static long InternalCurrentTimeMillis() => (long) (DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        private static long InternalCurrentTimeMillis() => (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+#endif
+
+#if NET6_0_OR_GREATER
+    private static long InternalCurrentTimeMillis() => (long)(DateTime.UtcNow - DateTime.UnixEpoch).TotalMilliseconds;
+#endif
+
     }
 }
