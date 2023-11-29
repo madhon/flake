22879a7cdd1cbe6f67c8f8265d035e49be688797
Madhon <madhon@madhon.co.uk>
2023-11-29 09:45:47 +0000

    build: Update packages and add .Net 8.0 support

--- Flake/InvalidSystemClockException.cs
+++ Flake/InvalidSystemClockException.cs
@@ -1,16 +1,10 @@
 ﻿namespace Flake
 {
     using System;
-    using System.Runtime.Serialization;
 
-    [Serializable]
     public class InvalidSystemClockException : Exception
     {
         public InvalidSystemClockException(string message) : base(message)
-        {
-        }
-
-        protected InvalidSystemClockException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
