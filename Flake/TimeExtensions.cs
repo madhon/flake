94a11060e070f00c7fafd78556ec251917246181
Madhon <madhon@madhon.co.uk>
2025-12-23 15:50:30 +0000

    feat: minor refactors

--- Flake/TimeExtensions.cs
+++ Flake/TimeExtensions.cs
@@ -1,26 +1,31 @@
 ﻿namespace Flake;
 
 using System;
+using System.Threading;
+
 
 public static class TimeExtensions
 {
-    private static TimeProvider currentTimeProvider = TimeProvider.System;
-
-    private static Func<long> currentTimeFunc = InternalCurrentTimeMillis;
+    private static readonly TimeProvider CurrentTimeProvider = TimeProvider.System;
+    private static volatile Func<long> currentTimeFunc = InternalCurrentTimeMillis;
 
     public static long CurrentTimeMillis() => currentTimeFunc();
 
     public static IDisposable StubCurrentTime(Func<long> func)
     {
-        currentTimeFunc = func;
-        return new DisposableAction(() => currentTimeFunc = InternalCurrentTimeMillis);
+        ArgumentNullException.ThrowIfNull(func);
+
+        // Swap atomically to be safe if used across threads
+        var previous = Interlocked.Exchange(ref currentTimeFunc, func);
+
+        return new DisposableAction(() => Interlocked.Exchange(ref currentTimeFunc, previous));
     }
 
     public static IDisposable StubCurrentTime(long millis)
     {
-        currentTimeFunc = () => millis;
-        return new DisposableAction(() => currentTimeFunc = InternalCurrentTimeMillis);
+        return StubCurrentTime(() => millis);
     }
 
-    private static long InternalCurrentTimeMillis() => currentTimeProvider.GetUtcNow().ToUnixTimeMilliseconds();
+    private static long InternalCurrentTimeMillis() =>
+        CurrentTimeProvider.GetUtcNow().ToUnixTimeMilliseconds();
 }
